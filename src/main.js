abf3652c5bd68aa92a3a791e27fbca1c02af6c29
moontai0724 <moontai0724@gmail.com>
2022-11-05 16:20:04 +0800

    style: reformat files

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import "./assets/main.css";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import router from "./router";
 
-import './assets/main.css'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(createPinia());
+app.use(router);
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
