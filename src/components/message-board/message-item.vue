2ad6a8e270975267e8580a391d573c2f4f93cc95
moontai0724 <moontai0724@gmail.com>
2022-11-09 19:47:40 +0800

    feat: message item & render items

--- src/components/message-board/message-item.vue
+++ src/components/message-board/message-item.vue
@@ -3,16 +3,18 @@
     class="card m-2 p-3 text-center flex-grow-1 flex-shrink-1 align-items-start justify-content-center align-items-center"
   >
     <blockquote class="blockquote mb-0">
-      <p class="content"></p>
+      <p class="content">{{ content }}</p>
       <footer class="blockquote-footer">
         <small class="text-muted">
-          <span class="author" title="留言者"></span>
+          <span class="author" title="留言者">{{ author }}</span>
           ＠
-          <span class="time" title="留言時間"></span>
-          <a class="user-select-none action edit">
+          <span class="time" title="留言時間">
+            {{ timeString }}
+          </span>
+          <a class="user-select-none action edit" @click="$emit('edit')">
             <i class="far fa-edit"></i>
           </a>
-          <a class="user-select-none action remove">
+          <a class="user-select-none action remove" @click="$emit('remove')">
             <i class="far fa-trash-alt"></i>
           </a>
         </small>
@@ -21,7 +23,38 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed, defineProps } from "vue";
+
+const props = defineProps({
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+});
+
+const timeString = computed(() =>
+  new Date(props.time).toLocaleString("zh-TW", {
+    hour12: false,
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  })
+);
+
+defineEmits(["edit", "remove"]);
+</script>
 
 <style scoped>
 .card p {
--- src/views/message-board.vue
+++ src/views/message-board.vue
@@ -13,7 +13,15 @@
       id="messages"
       class="d-flex flex-row flex-wrap justify-content-between text-center my-2"
     >
-      <!-- Messages here -->
+      <messageItemVue
+        v-for="(message, index) in messages"
+        :key="index"
+        :content="message.content"
+        :author="message.author"
+        :time="message.time"
+        @edit="openEditModal()"
+        @remove="openRemoveModal()"
+      ></messageItemVue>
     </div>
     <div class="d-grid gap-2">
       <button id="faker" type="button" class="btn btn-outline-secondary btn-lg">
@@ -24,7 +32,45 @@
   <!-- Modal here -->
 </template>
 
-<script setup></script>
+<script setup>
+import messageItemVue from "../components/message-board/message-item.vue";
+
+const messages = [
+  {
+    author: "author",
+    time: new Date().toISOString(),
+    content: "content",
+  },
+  {
+    author: "author",
+    time: new Date().toISOString(),
+    content: "content",
+  },
+  {
+    author: "author",
+    time: new Date().toISOString(),
+    content: "content",
+  },
+  {
+    author: "author",
+    time: new Date().toISOString(),
+    content: "content",
+  },
+  {
+    author: "author",
+    time: new Date().toISOString(),
+    content: "content",
+  },
+];
+
+function openEditModal() {
+  console.log("openEditModal");
+}
+
+function openRemoveModal() {
+  console.log("openRemoveModal");
+}
+</script>
 
 <style scoped>
 #messages:empty::before {
